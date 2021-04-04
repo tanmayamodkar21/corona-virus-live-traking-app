dd0745dcd64059a1d4ae6f9b48b0c66491ac6d52
THOR <tanuamodkar@gmail.com>
2021-04-04 15:36:16 +0530

    updated api

--- lib/network_requests/api_service.dart
+++ lib/network_requests/api_service.dart
@@ -1,9 +1,9 @@
-class ApiService{
-  final String _apiKey="apiKey=97d1baa2908948848db9c62c7d1ff94f";
-  final String _newsUrl="http://newsapi.org/v2/everything?";
-  final String _statsUrl="https://disease.sh/v2/";
-  final String _query="q=covid%2019%20vaccine";
-  final String _limit="pageSize=10&page=1";
+class ApiService {
+  final String _apiKey = "apiKey=93d588ff9ab931ede942712d76";
+  final String _newsUrl = "http://newsapi.org/v2/everything?";
+  final String _statsUrl = "https://disease.sh/v2/";
+  final String _query = "q=covid%2019%20vaccine";
+  final String _limit = "pageSize=10&page=1";
 
   String get apiKey => _apiKey;
   String get query => _query;
